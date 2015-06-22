8d8c547c039563c24acb2439b5fb8f87a54b4c6e
Jigar Shah <onlyjigar2772@gmail.com>
2015-06-22 23:10:41 +0530

    implemented email module, but password is blanked for safety now

--- newsletter/views.py
+++ newsletter/views.py
@@ -1,4 +1,6 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
+from django.conf import settings
 from .forms import ContactForm, SignUpForm
 # Create your views here.
 
@@ -29,12 +31,30 @@ def home(request):
 def contact(request):
     form = ContactForm(request.POST or None)
     if form.is_valid():
-        for key in form.cleaned_data:
-            print key
-        email = form.cleaned_data.get("email")
-        message = form.cleaned_data.get("message")
-        full_name = form.cleaned_data.get("full_name")
-        print email, message, full_name
+        # for key in form.cleaned_data:
+        #     print key
+        form_email = form.cleaned_data.get("email")
+        form_message = form.cleaned_data.get("message")
+        form_full_name = form.cleaned_data.get("full_name")
+        # print email, message, full_name
+        subject = 'Site contact form'
+        from_email = settings.EMAIL_HOST_USER
+        to_email = [from_email, 'atit.87@gmail.com']
+        contact_message = "%s: %s via %s" % (
+            form_full_name,
+            form_message,
+            form_email)
+
+        some_html_message = """
+        <h1>Welcome to the webpage</h1>
+        """
+
+        send_mail(subject,
+                  contact_message,
+                  from_email,
+                  to_email,
+                  html_message=some_html_message,
+                  fail_silently=True)
     context = {
         "form": form,
     }
--- travelclub/settings.py
+++ travelclub/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'onlyjigar2772@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Application definition
 
